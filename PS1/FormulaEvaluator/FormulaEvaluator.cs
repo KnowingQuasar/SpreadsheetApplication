2ee8da642dd8be141646ecd8b36cb896f934a0b3
Ian Rodriguez <irodriguez0456@gmail.com>
2016-09-05 20:55:12 -0600

    Initial Commit. First version of the evaluator for infix expressions. Tests to come.

---
+++ PS1/FormulaEvaluator/FormulaEvaluator.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Contains methods used to evaluate infix expressions and return an integer result.
+    /// </summary>
+    public static class Evaluator
+    {
+        /// <summary>
+        /// A Stack data structure used to hold expression numbers and variables. 
+        /// </summary>
+        private static Stack<string> operands = new Stack<string>();
+
+        /// <summary>
+        /// A Stack data Structure used to hold operators. Acceptable operators: *, /, +, -, (, ).
+        /// </summary>
+        private static Stack<string> operators = new Stack<string>();
+
+        /// <summary>
+        /// Array used to determine if a given token is a valid operator, and conversely used to tell if a token is a number.
+        /// </summary>
+        private static string[] allowedOperators = { "*", "/", "+", "-", "(", ")" };
+
+        /// <summary>
+        /// Delegate used to lookup the values of variables in expressions using the passed in method.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public delegate int Lookup(string value);
+
+        /// <summary>
+        /// This method is used to add operands together and remove them and the associated operator from the stack.
+        /// </summary>
+        private static void add()
+        {
+            operators.Pop();
+            int opResult = int.Parse(operands.Pop()) + int.Parse(operands.Pop());
+            operands.Push(opResult.ToString());
+        }
+
+        /// <summary>
+        /// This method is uesd to subtract one operand from another and remove them and the associated operator from the stack.
+        /// </summary>
+        private static void subtract()
+        {
+            operators.Pop();
+            int op1;
+            int op2;
+            op2 = int.Parse(operands.Pop());
+            op1 = int.Parse(operands.Pop());
+            int opResult = op1 - op2;
+            operands.Push(opResult.ToString());
+        }
+
+        /// <summary>
+        /// A method used for evaluating infix exprssions with respect to the standard order of operations. Accepts non-negative integers and variables. Legal operators: *, /, -, +, (, ).
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="variableEvaluator"></param>
+        /// <returns>Returns the result of evaluating the expression as an integer.</returns>
+        public static int Evaluate(string exp, Lookup variableEvaluator)
+        {
+            string exp_nowhitespace = Regex.Replace(exp, @"\s+", "");
+            string[] tokens = Regex.Split(exp_nowhitespace, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            foreach (string token in tokens)
+            {
+                if (token.Equals(""))
+                    continue;
+
+                int numberToken;
+                if (!allowedOperators.Contains(token))
+                {
+                    if (!(int.TryParse(token, out numberToken)))
+                        numberToken = variableEvaluator(token);
+                    if (operators.Count == 0)
+                    {
+                        operands.Push(token);
+                        continue;
+                    }
+                    else
+                    {
+                        int op1;
+                        if (operators.Peek().Equals("*"))
+                        {
+                            if (operands.Count == 0)
+                                throw new ArgumentException("Expression contains an illegal operation. There is no left-hand operand for the operation!");
+                            int.TryParse(operands.Pop(), out op1);
+                            operators.Pop();
+                            operands.Push((op1 * numberToken).ToString());
+                            continue;
+                        }
+                        else if (operators.Peek().Equals("/"))
+                        {
+                            if (operands.Count == 0)
+                                throw new ArgumentException("Expression contains an illegal operation. There is no left-hand operand for the operation!");
+                            if (numberToken == 0)
+                                throw new ArgumentException("Cannot execute expression - dividing by 0!");
+                            int.TryParse(operands.Pop(), out op1);
+                            operators.Pop();
+                            operands.Push((op1 / numberToken).ToString());
+                            continue;
+                        }
+                        else
+                            operands.Push(numberToken.ToString());
+                    }
+                }
+                else
+                {
+
+                    if (token.Equals("+") || token.Equals("-"))
+                    {
+                        if (operators.Count == 0)
+                            operators.Push(token);
+                        else
+                        {
+                            if (operators.Peek().Equals("+") || operators.Peek().Equals("-"))
+                            {
+                                if (operands.Count < 2)
+                                    throw new ArgumentException("Cannot execute expression - not enough arguments for the specified operators.");
+                                if (operators.Peek().Equals("+"))
+                                    add();
+                                else if (operators.Peek().Equals("-"))
+                                    subtract();
+                            }
+                            operators.Push(token);
+                        }
+                    }
+                    else if (token.Equals("*") || token.Equals("/") || token.Equals("("))
+                        operators.Push(token);
+                    else if (token.Equals(")"))
+                    {
+                        if (operators.Count == 0)
+                            throw new ArgumentException("Cannot execute expression - there is a \")\" without a matching \"(\".");
+                        else if (operators.Peek().Equals("+"))
+                            add();
+                        else if (operators.Peek().Equals("-"))
+                            subtract();
+
+                        if (operators.Count == 0)
+                            throw new ArgumentException("Cannot execute expression - there is a \")\" without a matching \"(\".");
+                        else if (!(operators.Peek().Equals("(")))
+                            throw new ArgumentException("Cannot execute expression - a \"(\" was not found in the expected location or is missing.");
+                        else
+                            operators.Pop();
+
+                        if (!(operators.Count == 0))
+                        {
+                            if (operators.Peek().Equals("*"))
+                            {
+                                operands.Push((int.Parse(operands.Pop()) * int.Parse(operands.Pop())).ToString());
+                                operators.Pop();
+                            }
+                            else if (operators.Peek().Equals("/"))
+                            {
+                                int op1;
+                                int op2;
+                                op2 = int.Parse(operands.Pop());
+                                if (op2 <= 0)
+                                    throw new ArgumentException("Cannot execute expression - one or more parts of this equation would result in a division by 0.");
+                                op1 = int.Parse(operands.Pop());
+                                operands.Push((op1 / op2).ToString());
+                                operators.Pop();
+                            }
+                        }
+                    }
+                }
+            }
+            if (operators.Count == 0 && operands.Count == 1)
+                return int.Parse(operands.Pop());
+            else
+            {
+                if (operands.Count == 0)
+                    throw new ArgumentException("Cannot execute expression - no expression has been given.");
+                if (!(operators.Count == 1) || !(operands.Count == 2))
+                    throw new ArgumentException("Cannot execute expression - the last operation does not have the needed amount of operands or operators.");
+                else if (operators.Peek().Equals("+"))
+                    add();
+                else if (operators.Peek().Equals("-"))
+                    subtract();
+                return int.Parse(operands.Pop());
+            }
+        }
+    }
+}
+
+
+
---
+++ PS1/FormulaEvaluatorTest/FormulaEvaluatorTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FormulaEvaluator;
+
+namespace FormulaEvaluatorTest
+{
+    class FormulaEvaluatorTest
+    {
+        private static bool TestEvaluator(int expected, int actual)
+        {
+            if (expected == actual)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string result;
+            string[] testExpressions =
+            {
+                //  "expression", //expected value
+                "2", // 2
+                "2 + 3", // 5
+                "3 - 2", // 1
+                "2 * 3", // 6
+                "4 / 2", // 2
+                "1 + 2 + 3", // 6
+                "2 + 3 - 1", // 4
+                "2 + 3 * 2", // 8
+                "2 + 3 / 3", // 3
+                "3 - 2 - 1", // 0
+                "3 - 2 + 1", // 2
+                "3 - 1 * 2", // 1
+                "3 - 2 / 1", // 1
+                "3 * 2 + 1", // 7
+                "3 * 2 - 1", // 5
+                "3 * 2 * 2", // 12
+                "3 * 2 / 2", // 3
+                "4 / 2 + 1", // 3
+                "4 / 2 - 1", // 1
+                "4 / 2 * 2", // 4
+                "4 / 2 / 2", // 1
+                "(2 + 3)", // 5
+                "(3 - 2)", // 1
+                "(2 * 3)", // 6
+                "(4 / 2)", // 2
+                "(1 + 2 + 3)", // 6
+                "(1 + 2 - 3)", // 0
+                "(1 + 2 * 3)", // 7
+                "(1 + 2 / 2)", // 2
+                "(1 - 2 + 3)", // 2
+                "(3 - 2 - 1)", // 0
+                "(3 - 2 * 1)", // 1
+                "(3 - 2 / 2)", // 2
+                "1 + (2 + 3)", // 6
+                "10 - (2 + 3)", // 5
+                "2 * (2 + 3)", // 10
+                "10 / (2 + 3)", // 2
+            };
+            TestEvaluator(2, Evaluator.Evaluate("(2)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("2 + 3", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("2 - 3", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.WriteLine(Evaluator.Evaluate("(0)", VarLookup));
+            Console.Read();
+        }
+
+        static int VarLookup(string val)
+        {
+            if (val.Equals("A3"))
+            {
+                return 2;
+            }
+            else return 5;
+        }
+
+    }
+}
